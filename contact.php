252b2b5a049be93134c01ea512aa5fa4cb7030f2
harshgujral <66208194+harshgujral@users.noreply.github.com>
2020-06-17 18:23:07 +0530

    kaliacoachingclasses

---
+++ contact.php
@@ -0,0 +1,24 @@
+
+<?php
+
+//variable setting
+$firstname = $_REQUEST['firstname'];
+$lastname = $_REQUEST['lastname'];
+$email = $_REQUEST['email'];
+$subject = $_REQUEST['subject'];
+$message= $_REQUEST['message'];
+
+
+//check input fields
+if (empty($firstname) || empty($lastname) || empty($email) || empty($subject) || empty($message))
+{
+	echo "please fill all the fields";
+}
+else 
+{
+	mail("Rishabkalia97@gmail.com","kalia coaching classes",$message,"From :$firstname,$lastname,$subject,$message<$email>");
+	echo "<script type='text/javascript'>alert('your message sent successfully');
+	window.history.log(-1);
+	</script>";
+}
+?>
